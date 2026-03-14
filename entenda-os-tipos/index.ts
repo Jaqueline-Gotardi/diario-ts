ac1b57daa544a0dc76ab3aeb208b859db8010c08
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-14 17:51:29 -0500

    feat: subindo mais exercícios de tipos e práticas da diferença entre tipagem explícita e inferência de tipo

--- entenda-os-tipos/index.ts
+++ entenda-os-tipos/index.ts
@@ -1,3 +1,16 @@
-let nome: string = "Jaque" //texto
+/* let nome: string = "Jaque" //texto
 let idade: number = 19; //números
-let mandouEmail: boolean = true; //booleanos => verdadeiro ou falso
+let mandouEmail: boolean = true; //booleanos => verdadeiro ou falso */
+
+/* let idades: number[] = [12, 22, 39]; //números
+let nomes: string[] = [ "Jaque", "Ribeiro", "Gotardi"] //nomes
+let idades: Array<number> = [12, 22, 39] //generics para arrays de números
+let obj: any = {x:0}; //any significa "qualquer tipo", mas não é mto recomendável usar! */
+
+function imprmirCoordenadas(pt: { x:number; y:number }) { //pt => parâmetro
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+imprmirCoordenadas({x: 3, y: 7})
+
+let obj: { x:number; y:number } = { x:3, y:7};
---
+++ tipagem-explicita-e-inferencia-de-tipo/index.ts
@@ -0,0 +1,7 @@
+// Tipagem Explícita: Quando atribuo o tipo
+/* let nome:  string = "Jaqueline";
+nome = 123; */
+
+// Inferência de tipo: Quando não atribuo o tipo, e o TS identifica sozinho o seu tipo
+/* let nome = "Jaqueline"
+nome = 123 */
