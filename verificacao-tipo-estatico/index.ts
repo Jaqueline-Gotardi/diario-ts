b3ef73382283a907967ebfa0e2bb1bed36c03160
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-15 14:39:32 -0500

    feat: subindo exercício de verificação de tipo estático

---
+++ verificacao-tipo-estatico/index.ts
@@ -0,0 +1,15 @@
+//VAI DAR ERRO PQ NOME NÃO É NUMBER, É STRING
+/* let nome = "Jaque";
+nome = 123; */
+
+
+//VAI DAR ERRO PQ A FUNCTION ESPERA DOIS NUMBERS, E NÃO UM
+/* function sum(number1: number, number2: number) {
+    return number1 + number2
+}
+sum(1); */
+
+
+//VAI DAR ERRO PQ CLASSLIST NÃO É UM MÉTODO DO MOVIES
+/* const movies = ["Baatman vs Superman", "Senhor dos Anéis"];
+movies.classList */
