5aadc84f14dc8ed2efc7316199f63883a407c054
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-15 14:39:59 -0500

    feat: subindo exercício de tuplas

---
+++ tuples/index.ts
@@ -0,0 +1,14 @@
+//ORDEM E TIPO IMPORTA
+/* const stringNumber: [string, number] = ["Jaque", 19]; */
+
+
+//ESTADOS TMB SÃO EXEMPLO DE TUPLAS
+/* const [name, setName] = useState("Jaque"); */
+
+//OUTRO EXEMPLO DE TUPLAS: PARA CADA TIPO -> UM VALOR
+/* let color: [number, number, number] = [255, 0, 0]; */
+
+
+//USAMOS "?" NAS TUPLAS PARA VALORES OPCIONAIS
+let bgColor: [number, number, number, number?];
+bgColor = [0, 255, 255];
